fb2cd2fc53455b86822adf5414da663769378b28
rimutaka <max@onebro.me>
2024-06-30 18:54:36 +1200

    Split crate and lambda name in deploy-proxy.sh

--- deploy-proxy.sh
+++ deploy-proxy.sh
@@ -1,7 +1,14 @@
-target=x86_64-unknown-linux-gnu 
+target=x86_64-unknown-linux-gnu
 region=us-east-1
-name=my-lambda
+lambda=my-lambda
+crate=proxy-lambda
 
 cargo build --release --target $target
-cp ./target/$target/release/proxy-lambda ./bootstrap && zip proxy.zip bootstrap && rm bootstrap
-aws lambda update-function-code --region $region --function-name $name --zip-file fileb://proxy.zip
+cp ./target/$target/release/$crate ./bootstrap && zip proxy.zip bootstrap && rm bootstrap
+aws lambda update-function-code --region $region --function-name $lambda --zip-file fileb://proxy.zip
+
+# Available targets: 
+# x86_64-unknown-linux-gnu
+# x86_64-unknown-linux-musl
+# aarch64-unknown-linux-gnu
+# aarch64-unknown-linux-musl
